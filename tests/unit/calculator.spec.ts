75f17028f27776d1a4480ce730f8f359b76a539a
tarunsingh-eng <tarun.xpe@gmail.com>
2026-02-22 21:13:03 +0530

    unit tests

--- tests/unit/calculator.spec.ts
+++ tests/unit/calculator.spec.ts
@@ -13,3 +13,11 @@ test('basic url test', async ({ page }) => {
     await page.goto("https://www.tarunsingh.co.in");
     await expect(page).toHaveTitle(/Tarun/i);
 })
+
+test('multiple number', () => {
+    expect (calc.multiply(2,5)).toBe(10)
+})
+
+test('divide number', () => {
+    expect(calc.divide(10,2)).toBe(5)
+})
