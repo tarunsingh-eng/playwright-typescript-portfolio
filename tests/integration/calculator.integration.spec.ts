2884ad43846f441c8a4241dd34f8a9759a989d7a
tarunsingh-eng <tarun.xpe@gmail.com>
2026-03-11 12:15:11 +0530

    multiple integration tests added

--- tests/integration/calculator.integration.spec.ts
+++ tests/integration/calculator.integration.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { assert } from 'node:console';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/index.html');
@@ -39,4 +40,58 @@ test('devide two numbers', async ({page}) => {
   await page.click('button[onclick="calculate()"]');
 
   await expect.soft(page.locator('div[id="result"]')).toHaveText('Result: 25');
+})
+
+test('Multiple multiply operations + use of clear display button', async ({ page }) => {
+  await page.fill('#num1','30');
+  await page.fill('#num2','20');
+  await page.click('button[onclick="input(\'multiply\')"]');
+  await page.click('button[onclick="calculate()"]');
+
+  await expect.soft(page.locator('#result')).toHaveText('Result: 600');
+
+  await page.click('button[onclick="clearDisplay()"]');
+
+  await page.fill('#num1','40');
+  await page.fill('#num2','50');
+  await page.click('button[onclick="input(\'multiply\')"]');
+  await page.click('button[onclick="calculate()"]');
+
+  await expect.soft(page.locator('#result')).toHaveText('Result: 2000');
+})
+
+test('multiple operations: add, substract, multiply and divide', async({page})=> {
+    await page.fill('#num1', '20');
+    await page.fill('#num2', '2');
+    await page.click('button[onclick="input(\'add\')"]');
+    await page.click('button[onclick="calculate()"]');
+
+    await expect(page.locator('#result')).toHaveText('Result: 22');
+    console.log("Number added");
+
+    await page.fill('#num2', '20');
+    await page.click('button[onclick="input(\'divide\')"]');
+    await page.click('button[onclick="calculate()"]');
+
+    await expect(page.locator('#result')).toHaveText('Result: 1');
+    console.log("Number divided");
+
+    await page.fill('#num1', '3');
+    await page.click('button[onclick="input(\'subtract\')"]');
+    await page.click('button[onclick="calculate()"]')
+
+    await expect(page.locator('#result')).toHaveText('Result: -17');
+    console.log("Number Substracted");
+
+    await page.fill('#num2', '1.5');
+    await page.click('button[onclick="input(\'multiply\')"]');
+    await page.click('button[onclick="calculate()"]');
+
+    await expect(page.locator('#result')).toHaveText('Result: 4.5');
+    console.log("number multiplied")
+
+    await page.click('button[onclick="clearDisplay()"]');
+
+    await expect(page.locator('#display')).toHaveText('');
+    console.log("Display Cleared");
 })
