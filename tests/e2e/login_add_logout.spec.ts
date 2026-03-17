b676071f4ffab5e015422512af4291322a5b1a27
tarunsingh-eng <tarun.xpe@gmail.com>
2026-03-17 15:29:27 +0530

    added e2e flow test

---
+++ tests/e2e/login_add_logout.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '../fixtures/auth.fixture';
+
+test.beforeEach(async ({ loggedInPage }) => {
+  await loggedInPage.waitForSelector('#num1');
+});
+
+
+test ('E2E flow 1: login, user calculator, and logout', async ({loggedInPage}) => {
+    
+    await loggedInPage.fill('#num1','2');
+    await loggedInPage.fill('#num2','5');
+    await loggedInPage.click('button[onclick="input(\'multiply\')"]');
+    await loggedInPage.click('button[onclick="calculate()"]');
+
+    await expect(loggedInPage.locator('#result')).toHaveText('Result: 10');
+
+    await loggedInPage.click('#logoutBtn');
+
+});
+
