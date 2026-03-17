5db3e9507e4af713871149e4a31c43910eed5296
tarunsingh-eng <tarun.xpe@gmail.com>
2026-03-17 14:19:25 +0530

    login added

---
+++ tests/fixtures/auth.fixture.ts
@@ -0,0 +1,23 @@
+//fixture/auth.fixture.ts
+import {test as base,Page}from '@playwright/test';
+
+type MyFixtures= {
+    loggedInPage: Page;
+};
+
+export const test = base.extend<MyFixtures> ({
+    loggedInPage: async ({page}, use) => {
+        await page.goto('/index.html');
+
+        await page.fill('#username', 'admin');
+        await page.fill('#password', '1234');
+        
+        await Promise.all([
+         page.waitForURL('**/calculator'),
+         page.click('#loginBtn'),
+        ]);
+        await use(page);
+    },
+});
+
+export { expect } from '@playwright/test';
--- tests/integration/calculator.integration.spec.ts
+++ tests/integration/calculator.integration.spec.ts
@@ -1,140 +1,140 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '../fixtures/auth.fixture';
 
-
-test.beforeEach(async ({ page }) => {
-  await page.goto('/index.html');
-  await page.waitForSelector('#num1');
+test.beforeEach(async ({ loggedInPage }) => {
+  // await loggedInPage.goto('/index.html');
+  // await loggedInPage.goto('/calculator.html');
+  await loggedInPage.waitForSelector('#num1');
 });
 
-test('adds two numbers', async ({ page }) => {
-  await page.fill('#num1', '2');
-  await page.fill('#num2', '3');
-  await page.click('button[onclick="input(\'add\')"]');
-  await page.click('button[onclick="calculate()"]');
+test('adds two numbers', async ({ loggedInPage }) => {
+  await loggedInPage.fill('#num1', '2');
+  await loggedInPage.fill('#num2', '3');
+  await loggedInPage.click('button[onclick="input(\'add\')"]');
+  await loggedInPage.click('button[onclick="calculate()"]');
 
-  await expect(page.locator('#result')).toHaveText('Result: 5');
+  await expect(loggedInPage.locator('#result')).toHaveText('Result: 5');
 });
 
-test('substract two numbers', async ({page}) => {
-  await page.fill('#num1', '5');
-  await page.fill('#num2', '2');
-  await page.click('button[onclick="input(\'subtract\')"]');
-  await page.click('button[onclick="calculate()"]');
+test('subtract two numbers', async ({loggedInPage}) => {
+  await loggedInPage.fill('#num1', '5');
+  await loggedInPage.fill('#num2', '2');
+  await loggedInPage.click('button[onclick="input(\'subtract\')"]');
+  await loggedInPage.click('button[onclick="calculate()"]');
 
-  await expect.soft(page.locator('#result')).toHaveText('Result: 3');
+  await expect.soft(loggedInPage.locator('#result')).toHaveText('Result: 3');
 });
 
-test('multiply two numbers', async ({page}) => {
-  await page.fill("#num1", '5');
-  await page.fill("#num2",'4');
-  await page.click('button[onclick="input(\'multiply\')"]');
-  await page.click('button[onclick="calculate()"]');
+test('multiply two numbers', async ({loggedInPage}) => {
+  await loggedInPage.fill("#num1", '5');
+  await loggedInPage.fill("#num2",'4');
+  await loggedInPage.click('button[onclick="input(\'multiply\')"]');
+  await loggedInPage.click('button[onclick="calculate()"]');
 
-  await expect.soft(page.locator('#result')).toHaveText('Result: 20');
+  await expect.soft(loggedInPage.locator('#result')).toHaveText('Result: 20');
 });
 
 
-test('devide two numbers', async ({page}) => {
-  await page.fill('input[placeholder="Number 1"]','50');
-  await page.fill('input[placeholder="Number 2"]', '2');
-  await page.click('button[onclick="input(\'divide\')"]');
-  await page.click('button[onclick="calculate()"]');
+test('divide two numbers', async ({loggedInPage}) => {
+  await loggedInPage.fill('input[placeholder="Number 1"]','50');
+  await loggedInPage.fill('input[placeholder="Number 2"]', '2');
+  await loggedInPage.click('button[onclick="input(\'divide\')"]');
+  await loggedInPage.click('button[onclick="calculate()"]');
 
-  await expect.soft(page.locator('div[id="result"]')).toHaveText('Result: 25');
+  await expect.soft(loggedInPage.locator('div[id="result"]')).toHaveText('Result: 25');
 })
 
-test('Multiple multiply operations + use of clear display button', async ({ page }) => {
-  await page.fill('#num1','30');
-  await page.fill('#num2','20');
-  await page.click('button[onclick="input(\'multiply\')"]');
-  await page.click('button[onclick="calculate()"]');
+test('Multiple multiply operations + use of clear display button', async ({ loggedInPage }) => {
+  await loggedInPage.fill('#num1','30');
+  await loggedInPage.fill('#num2','20');
+  await loggedInPage.click('button[onclick="input(\'multiply\')"]');
+  await loggedInPage.click('button[onclick="calculate()"]');
 
-  await expect.soft(page.locator('#result')).toHaveText('Result: 600');
+  await expect.soft(loggedInPage.locator('#result')).toHaveText('Result: 600');
 
-  await page.click('button[onclick="clearDisplay()"]');
+  await loggedInPage.click('button[onclick="clearDisplay()"]');
 
-  await page.fill('#num1','40');
-  await page.fill('#num2','50');
-  await page.click('button[onclick="input(\'multiply\')"]');
-  await page.click('button[onclick="calculate()"]');
+  await loggedInPage.fill('#num1','40');
+  await loggedInPage.fill('#num2','50');
+  await loggedInPage.click('button[onclick="input(\'multiply\')"]');
+  await loggedInPage.click('button[onclick="calculate()"]');
 
-  await expect.soft(page.locator('#result')).toHaveText('Result: 2000');
+  await expect.soft(loggedInPage.locator('#result')).toHaveText('Result: 2000');
 })
 
-test('multiple operations: add, substract, multiply, divide and clear display', async({page})=> {
-    await page.fill('#num1', '20');
-    await page.fill('#num2', '2');
-    await page.click('button[onclick="input(\'add\')"]');
-    await page.click('button[onclick="calculate()"]');
+test('multiple operations: add, substract, multiply, divide and clear display', async({loggedInPage})=> {
+    await loggedInPage.fill('#num1', '20');
+    await loggedInPage.fill('#num2', '2');
+    await loggedInPage.click('button[onclick="input(\'add\')"]');
+    await loggedInPage.click('button[onclick="calculate()"]');
 
-    await expect(page.locator('#result')).toHaveText('Result: 22');
+    await expect(loggedInPage.locator('#result')).toHaveText('Result: 22');
     console.log("Number added");
 
-    await page.fill('#num2', '20');
-    await page.click('button[onclick="input(\'divide\')"]');
-    await page.click('button[onclick="calculate()"]');
+    await loggedInPage.fill('#num2', '20');
+    await loggedInPage.click('button[onclick="input(\'divide\')"]');
+    await loggedInPage.click('button[onclick="calculate()"]');
 
-    await expect(page.locator('#result')).toHaveText('Result: 1');
+    await expect(loggedInPage.locator('#result')).toHaveText('Result: 1');
     console.log("Number divided");
 
-    await page.fill('#num1', '3');
-    await page.click('button[onclick="input(\'subtract\')"]');
-    await page.click('button[onclick="calculate()"]')
+    await loggedInPage.fill('#num1', '3');
+    await loggedInPage.click('button[onclick="input(\'subtract\')"]');
+    await loggedInPage.click('button[onclick="calculate()"]')
 
-    await expect(page.locator('#result')).toHaveText('Result: -17');
+    await expect(loggedInPage.locator('#result')).toHaveText('Result: -17');
     console.log("Number Substracted");
 
-    await page.fill('#num2', '1.5');
-    await page.click('button[onclick="input(\'multiply\')"]');
-    await page.click('button[onclick="calculate()"]');
+    await loggedInPage.fill('#num2', '1.5');
+    await loggedInPage.click('button[onclick="input(\'multiply\')"]');
+    await loggedInPage.click('button[onclick="calculate()"]');
 
-    await expect(page.locator('#result')).toHaveText('Result: 4.5');
+    await expect(loggedInPage.locator('#result')).toHaveText('Result: 4.5');
     console.log("number multiplied")
 
-    await page.click('button[onclick="clearDisplay()"]');
+    await loggedInPage.click('button[onclick="clearDisplay()"]');
 
-    await expect(page.locator('#display')).toHaveText('');
+    await expect(loggedInPage.locator('#display')).toHaveText('');
     console.log("Display Cleared");
 })
 
 
-test('multiply by result', async ({page}) => {
+test('multiply by result', async ({loggedInPage}) => {
 
-    await page.fill('#num1','10');
-    await page.fill('#num2','300');
-    await page.click('button[onclick="input(\'multiply\')"]');
-    await page.click('button[onclick="calculate()"]');
+    await loggedInPage.fill('#num1','10');
+    await loggedInPage.fill('#num2','300');
+    await loggedInPage.click('button[onclick="input(\'multiply\')"]');
+    await loggedInPage.click('button[onclick="calculate()"]');
 
-    await expect(page.locator('#result')).toHaveText('Result: 3000');
+    await expect(loggedInPage.locator('#result')).toHaveText('Result: 3000');
 
-    let str1:string = await page.locator('#result').textContent();
+    let str1:string = await loggedInPage.locator('#result').textContent();
 
-    await page.fill('#num2', str1.slice(8));
-    await page.click('button[onclick="calculate()"]');
+    await loggedInPage.fill('#num2', str1.slice(8));
+    await loggedInPage.click('button[onclick="calculate()"]');
 
-    await expect(page.locator('#result')).toHaveText('Result: 30000');
-
-})
-
-
-test("add test", async({page})=> {
-  await page.fill('#num1','20');
-  await page.fill('#num2','10');
-  await page.click('button[onclick="input(\'add\')"]');
-  await page.click('button[onclick="calculate()"]');
-
-  await expect(page.locator('#result')).toHaveText('Result: 30');
+    await expect(loggedInPage.locator('#result')).toHaveText('Result: 30000');
 
 })
 
 
-test("divide by zero", async ({page}) => {
-  await page.fill('#num1','20');
-  await page.fill('#num2','0');
-  await page.click('button[onclick="input(\'divide\')"]');
-  await page.click('button[onclick="calculate()"]');
+test("add test", async({loggedInPage})=> {
+  await loggedInPage.fill('#num1','20');
+  await loggedInPage.fill('#num2','10');
+  await loggedInPage.click('button[onclick="input(\'add\')"]');
+  await loggedInPage.click('button[onclick="calculate()"]');
 
-  await expect(page.locator('#result')).toHaveText('Division by zero');
+  await expect(loggedInPage.locator('#result')).toHaveText('Result: 30');
+
+})
+
+
+test("divide by zero", async ({loggedInPage}) => {
+  await loggedInPage.fill('#num1','20');
+  await loggedInPage.fill('#num2','0');
+  await loggedInPage.click('button[onclick="input(\'divide\')"]');
+  await loggedInPage.click('button[onclick="calculate()"]');
+
+  await expect(loggedInPage.locator('#result')).toHaveText('Division by zero');
 
 }
 )
