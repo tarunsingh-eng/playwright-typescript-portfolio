a520ff1fe4c20da425b356642ed02aa5fccc1dca
tarunsingh-eng <tarun.xpe@gmail.com>
2026-03-31 16:30:11 +0530

    added usertest_auth

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,4 +1,5 @@
 import {test, expect} from '@playwright/test';
+import { request } from 'node:http';
 
 test('Get local calculator page return 200 and shows title text', async ({ request}) => {
 
@@ -9,3 +10,16 @@ expect(response.status()).toBe(200);
 const body =await response.text();
 expect(body).toContain('calculator');
 } );
+
+
+test('calculator page requires authentication', async({request}) => {
+
+    const res = await request.get('/calculator.html');
+
+    expect(res.status()).toBe(200);
+
+    const body = await res.text();
+
+    expect(body).toContain('localStorage.getItem("user")');
+});
+
